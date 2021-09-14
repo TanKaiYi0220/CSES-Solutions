4117999cf166aec674ca4bd22d4c26a879311eb0
TanKaiYi <60723049+TanKaiYi0220@users.noreply.github.com>
2021-09-14 21:48:01 +0800

    Solution Update

--- src/Introductory Problems/Increasing Array.cpp
+++ src/Introductory Problems/Increasing Array.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
 	int n;
 	cin >> n;
-	int nums[n];
 	int maxN = 0;
 	long long ans = 0;
 	for ( int i = 0 ; i < n ; i++ )
 	{
-		cin >> nums[i];
-		if ( nums[i] < maxN ) ans += maxN - nums[i];
-		maxN = max(maxN, nums[i]);
+		int x;
+		cin >> x;
+		if ( x < maxN ) ans += maxN - x;
+		maxN = max(maxN, x);
 	}
 	cout << ans;
 }
